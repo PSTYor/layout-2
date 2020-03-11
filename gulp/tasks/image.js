9aa53c236c03c74e937fde81351d7bc252f51059
Сергей Старков <svictorovich999@gmail.com>
2020-03-11 18:33:52 +0500

    Добавил SVG-изображения. Создал таск для обработки SVG. Подкорректировал SCSS и Pug файлы

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     $.gulp.task('image', function() {
-        return $.gulp.src('./app/img/*')
+        return $.gulp.src('./app/img/*.{png}{jpg}')
             .pipe($.plugins.imagemin())
             .pipe($.gulp.dest('./dist/img'))
             .pipe($.browserSync.reload({stream:true}));
