169a8663f8afd11c656e275aea77fdd7e7d7664e
Сергей Старков <svictorovich999@gmail.com>
2020-03-13 13:14:00 +0500

    Добавил блок "Мои проекты" с Gulp, SCSS, изображением. Отредактировал таски, и миксины

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     $.gulp.task('image', function() {
-        return $.gulp.src('./app/img/*.{png}{jpg}')
+        return $.gulp.src('./app/img/*.+(jpg|png)')
             .pipe($.plugins.imagemin())
             .pipe($.gulp.dest('./dist/img'))
             .pipe($.browserSync.reload({stream:true}));
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,7 @@ module.exports = function() {
     $.gulp.task('watch', function() {
         $.gulp.watch('./app/sass/**/*.scss', $.gulp.series('sass'));
         $.gulp.watch('./app/pug/**/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('./app/img/*', $.gulp.series('image'));
+        $.gulp.watch('./app/img/*.+(png|jpg)', $.gulp.series('image'));
+        $.gulp.watch('./app/img/svg/*.svg', $.gulp.series('svg'));
     });
 };
